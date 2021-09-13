7e0a91ed287c569fca85c8f1f262b4628df9f923
isandratskiy <i.sandrackii@gmail.com>
2021-09-13 04:42:31 +0300

    added Spark with input data

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.apache.spark:spark-core_2.12:3.1.2")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
