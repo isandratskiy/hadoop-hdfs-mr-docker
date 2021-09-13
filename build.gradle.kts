6ab1d04fdba281d4d02d30b7b30903f573ca19f0
isandratskiy <i.sandrackii@gmail.com>
2021-09-14 01:28:41 +0300

    added data_set

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,10 @@ repositories {
 }
 
 dependencies {
+    implementation("org.apache.spark:spark-sql_2.12:3.1.2")
     implementation("org.apache.spark:spark-core_2.12:3.1.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
 
---
+++ src/test/java/spark/SparkDataSet.java
@@ -0,0 +1,56 @@
+package spark;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.StructType;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.apache.spark.sql.SparkSession.builder;
+import static org.apache.spark.sql.types.DataTypes.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DisplayName("Spark DataSet")
+public class SparkDataSet {
+    SparkSession spark;
+    Dataset<Row> ds;
+
+    @BeforeEach
+    void before() {
+        this.spark = builder().master("local[*]").appName("Spark_DataSet").getOrCreate();
+
+        var schema = new StructType()
+                .add("Year", IntegerType, true)
+                .add("Industry_aggregation_NZSIOC", StringType, true)
+                .add("Industry_code_NZSIOC", IntegerType, true)
+                .add("Industry_name_NZSIOC", StringType, true)
+                .add("Units", StringType, true)
+                .add("Variable_code", StringType, true)
+                .add("Variable_name", StringType, true)
+                .add("Variable_category", StringType, true)
+                .add("Value", DateType, true)
+                .add("Industry_code_ANZSIC06", StringType, true);
+
+        this.ds = spark.read().format("csv")
+                .option("header", "true")
+                .option("delimiter", ",")
+                .schema(schema)
+                .load("data/annual-enterprise-survey-2020-financial-year-provisional-csv.csv");
+    }
+
+    @AfterEach
+    void close() {
+        spark.stop();
+    }
+
+    @DisplayName("should count all values")
+    @Test
+    void shouldCount() {
+        this.ds = ds.select("Variable_name").filter("Variable_name = 'Indirect taxes'");
+        this.ds.show(50);
+        assertEquals(1112, this.ds.count());
+    }
+}
