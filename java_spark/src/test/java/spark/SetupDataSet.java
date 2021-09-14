fb32f4b97ec701161e8e5c1c7cda3de722657167
isandratskiy <i.sandrackii@gmail.com>
2021-09-14 15:08:40 +0300

    clean up, added dataset extension

---
+++ java_spark/src/test/java/spark/SetupDataSet.java
@@ -0,0 +1,55 @@
+package spark;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.StructType;
+import org.junit.jupiter.api.extension.*;
+
+import static org.apache.spark.sql.SparkSession.builder;
+import static org.apache.spark.sql.types.DataTypes.IntegerType;
+import static org.apache.spark.sql.types.DataTypes.StringType;
+import static org.junit.jupiter.api.extension.ExtensionContext.Namespace.GLOBAL;
+
+public class SetupDataSet implements BeforeAllCallback, ParameterResolver, AfterEachCallback {
+
+    @Override
+    public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+        return parameterContext.getParameter().getType() == Dataset.class;
+    }
+
+    @Override
+    public Dataset resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+        return extensionContext.getStore(GLOBAL).get("DATA_SET", Dataset.class);
+    }
+
+    @Override
+    public void beforeAll(ExtensionContext context) {
+        var spark = builder().master("local[*]").appName("Spark_DataSet").getOrCreate();
+
+        var schema = new StructType()
+                .add("Year", IntegerType, true)
+                .add("Industry_aggregation_NZSIOC", StringType, true)
+                .add("Industry_code_NZSIOC", StringType, true)
+                .add("Industry_name_NZSIOC", StringType, true)
+                .add("Units", StringType, true)
+                .add("Variable_code", StringType, true)
+                .add("Variable_name", StringType, true)
+                .add("Variable_category", StringType, true)
+                .add("Value", StringType, true)
+                .add("Industry_code_ANZSIC06", StringType, true);
+
+        var ds = spark.read().format("csv")
+                .option("header", "true")
+                .option("delimiter", ",")
+                .schema(schema)
+                .load("src/test/resources/annual-enterprise-survey-2020-financial-year-provisional-csv.csv");
+
+        context.getStore(GLOBAL).put("DATA_SET", ds);
+        context.getStore(GLOBAL).put("SPARK_SESSION", spark);
+    }
+
+    @Override
+    public void afterEach(ExtensionContext context) {
+        context.getStore(GLOBAL).get("SPARK_SESSION", SparkSession.class).stop();
+    }
+}
--- java_spark/src/test/java/spark/SparkDataSet.java
+++ java_spark/src/test/java/spark/SparkDataSet.java
@@ -1,57 +1,28 @@
 package spark;
 
 import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.types.StructType;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
-import static org.apache.spark.sql.SparkSession.builder;
-import static org.apache.spark.sql.types.DataTypes.IntegerType;
-import static org.apache.spark.sql.types.DataTypes.StringType;
+import static org.apache.spark.sql.functions.col;
+import static org.apache.spark.sql.functions.regexp_replace;
+import static org.apache.spark.sql.types.DataTypes.DoubleType;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DisplayName("Spark DataSet")
+@ExtendWith(SetupDataSet.class)
 public class SparkDataSet {
-    SparkSession spark;
-    Dataset<Row> ds;
 
-    @BeforeEach
-    void before() {
-        this.spark = builder().master("local[*]").appName("Spark_DataSet").getOrCreate();
-
-        var schema = new StructType()
-                .add("Year", IntegerType, true)
-                .add("Industry_aggregation_NZSIOC", StringType, true)
-                .add("Industry_code_NZSIOC", StringType, true)
-                .add("Industry_name_NZSIOC", StringType, true)
-                .add("Units", StringType, true)
-                .add("Variable_code", StringType, true)
-                .add("Variable_name", StringType, true)
-                .add("Variable_category", StringType, true)
-                .add("Value", StringType, true)
-                .add("Industry_code_ANZSIC06", StringType, true);
-
-        this.ds = spark.read().format("csv")
-                .option("header", "true")
-                .option("delimiter", ",")
-                .schema(schema)
-                .load("src/test/resources/annual-enterprise-survey-2020-financial-year-provisional-csv.csv");
-    }
-
-    @AfterEach
-    void close() {
-        spark.stop();
-    }
-
-    @DisplayName("should count all values")
     @Test
-    void shouldCount() {
-        this.ds = ds.filter("Variable_name = 'Indirect taxes'");
-        this.ds.show(30);
-        assertEquals(1112, this.ds.count());
+    @DisplayName("should sum all 'taxes' values")
+    void shouldCount(Dataset<String> ds) {
+        var filteredDs = ds.filter(col("Variable_name").contains("taxes"))
+                .withColumn("Value", regexp_replace(col("Value"), ",", "."))
+                .withColumn("Value", col("Value").cast(DoubleType));
+        var groupedDs = filteredDs.groupBy("Variable_name").sum("Value");
+        var sum = groupedDs.first().getDouble(1);
+        groupedDs.show();
+        assertEquals(73819.69, sum);
     }
 }
